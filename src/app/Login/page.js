0fe6e506bad9a87da1e7d30fdd94b34331067738
Pedro Serpe <pserpe01@hotmail.com>
2023-12-12 01:18:07 +0000

    Ponto de partida

---
+++ src/app/Login/page.js
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <h1>Página do login em construção</h1>
+}
---
+++ src/app/administrador/page.js
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <h1>Página do administrador em construção</h1>
+}
