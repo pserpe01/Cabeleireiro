9c4a22221e1576d60b8c737c63b4237ca8a99ecc
Pedro Serpe <pserpe01@hotmail.com>
2023-12-14 19:21:12 +0000

    Styling page

--- src/app/administrador/page.js
+++ src/app/administrador/page.js
@@ -1,3 +1,13 @@
-export default function Page(){
-    return <h1>Página do administrador em construção</h1>
+import styles from '../styles/adminLayout.module.css'
+
+export default function AdminLayout() {
+    return(
+        <html lang='en'>
+            <body>
+                <div className={styles.warning}>
+                <h1>Em construção</h1>
+                </div>
+            </body>
+        </html>
+    )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,3 @@
-import Image from 'next/image'
-import styles from './styles/page.module.css'
-
 export default function Home() {
-  return (
-
-  <h1 className='boas'>Página principal em construção</h1>)
+  return (<h1 className='boas'>Página principal em construção</h1>)
 }
