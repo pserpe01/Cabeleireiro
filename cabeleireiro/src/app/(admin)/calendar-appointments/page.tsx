3cba002073dec5f01f4181e76c4464267fe0afc1
Pedro Serpe <pserpe01@hotmail.com>
2023-12-15 01:33:30 +0000

    Routes atualizadas
    Atualização das routes;
    Criação do ficheiro not-found.

---
+++ cabeleireiro/src/app/(admin)/calendar-appointments/page.tsx
@@ -0,0 +1,5 @@
+export default function CalendarAppointments() {
+    return (
+      <h1>Calendar Appointments</h1>
+    )
+  }
---
+++ cabeleireiro/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return(
+        <div>
+            <h2>Página não encontrada</h2>
+        </div>
+    )
+}
